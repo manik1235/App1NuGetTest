9e1fb8f09b875d00d01fcab1e977acfe27e7ea72
Andrew Henning-Kolberg <andrew.kolberg@gmail.com>
2017-10-18 23:33:52 -0400

    built solution after adding NuGet, added example code, haven't run it yet

--- App1NuGetTest/MainPage.xaml.cs
+++ App1NuGetTest/MainPage.xaml.cs
@@ -26,5 +26,25 @@ namespace App1NuGetTest
         {
             this.InitializeComponent();
         }
+
+        public class Account
+        {
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public DateTime DOB { get; set; }
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Account account = new Account
+            {
+                Name = "John Doe",
+                Email = "john@hksauce.com",
+                DOB = new DateTime(1980, 2, 20, 0, 0, 0, DateTimeKind.Utc),
+            };
+            string json = JsonConvert.SerializeObject(account, Formatting.Indented);
+            TextBlock.Text = json;
+        }
     }
 }
